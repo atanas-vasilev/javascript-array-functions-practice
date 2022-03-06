b98d59dfd10bc1fa0c87602e109ba87262f56d9c
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2022-03-06 11:39:11 +0000

    Solved .reduce()

--- worksheet.js
+++ worksheet.js
@@ -32,22 +32,39 @@ const characters = [
 //***MAP***
 //1. Get array of all names
 const names = characters.map(char => char.name)
-console.log(names)
+// console.log(names)
 //2. Get array of all heights
 const heights = characters.map(char => char.height)
-console.log(heights)
+// console.log(heights)
 //3. Get array of objects with just name and height properties
 const arrNameHeight = characters.map(char => ({name:char.name, height:char.height}))
-console.table(arrNameHeight)
+// console.table(arrNameHeight)
 //4. Get array of all first names
 const firstNames = characters.map(char => char.name.substr(0, char.name.indexOf(' ')));
-console.log(firstNames);
+// console.log(firstNames);
 
 //***REDUCE***
 //1. Get total mass of all characters
+const totalMass = characters.reduce((total, char) => char.mass+total,0);
+console.log(totalMass);
 //2. Get total height of all characters
+const totalHeight = characters.reduce((total, char) => char.height+total,0);
+console.log(totalHeight);
 //3. Get total number of characters by eye color
+const charsEyeColor = characters.reduce((acc, currentCharacter) =>{
+    if(!(currentCharacter.eye_color in acc)){
+        acc[currentCharacter.eye_color] = 1;
+    } else{
+        acc[currentCharacter.eye_color]++;
+    }
+    return acc;
+},{})
+console.table(charsEyeColor);
 //4. Get total number of characters in all the character names
+const totalCharsInNames = characters.reduce((total, currentCharacter) => total += currentCharacter.name.length,0);
+console.log(totalCharsInNames);
+
+
 
 //***FILTER***
 //1. Get characters with mass greater than 100
